4b376f51873b756723334540c696a69405a2bc45
ALEX <serkov.gcnet@mail.ru>
2024-07-31 21:40:14 +0300

    Internet store project

---
+++ ShopWave/src/test/java/ShopWave/ShopWave/ShopWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package ShopWave.ShopWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
